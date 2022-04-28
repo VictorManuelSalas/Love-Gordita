f782a722ed9539ab3d10af6fbde043371e822706
VictorManuelSalas <victorsalasnz@gmail.com>
2022-04-28 16:09:44 -0500

    GoritaNew

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 document.getElementById('pagar').onclick = function () {
     alert("¡¡¡ Se realizo la acompra correctamente, en 30 minutos un repartidor llegara a su domicilio !!!");
 };
+
+
+
---
+++ main2.js
@@ -0,0 +1,3 @@
+document.getElementById('agregar').onclick = function () {
+    alert("¡¡¡ Se añadio su pedido al carrito de compras, para realizar el pedido porfavor seleccione la opcion del carrito para pagar su encargo !!!");
+};
